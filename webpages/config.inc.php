fb12a12e65c6cc97589084b760aa05e1b2d9e1d8
Christopher-Nottingham <cnott729@mtroyal.ca>
2023-10-05 19:32:59 -0600

    Organizing Repo

--- webpages/config.inc.php
+++ webpages/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 define('DBHOST', 'localhost');
-	
-	define('DBNAME', './databases/music.db');
+	//./databases/
+	//define('DBNAME', 'music.db');
  define('DBUSER', 'root');
 	define('DBPASS', '');
-define('DBCONNSTRING','sqlite:./databases/music.db');
+define('DBCONNSTRING','sqlite:../databases/music.db');
 // define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME .
 // ";charset=utf8mb4;");
 ?>
